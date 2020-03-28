656cceb9889aba2f484bbd9f9d0bd43581e05227
Oswald Maximilian <45564897+oswmam16@users.noreply.github.com>
2020-03-28 15:52:56 +0100

    Create MyThread2204.java

---
+++ java-corona-OswaldMaxi/src/k22/k22_3/k22_3_1/MyThread2204.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package k22.k22_3.k22_3_1;
+
+/**
+ *
+ * @author maxio
+ */
+public class MyThread2204 {
+    
+}
