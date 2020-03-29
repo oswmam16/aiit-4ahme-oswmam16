0b550047cbb86c5bb2e628be6b413158c9aec2b9
Oswald Maximilian <45564897+oswmam16@users.noreply.github.com>
2020-03-29 16:05:21 +0200

    Update MyThread2204.java

--- java-corona-OswaldMaxi/src/k22/k22_3/k22_3_1/MyThread2204.java
+++ java-corona-OswaldMaxi/src/k22/k22_3/k22_3_1/MyThread2204.java
@@ -9,6 +9,31 @@ package k22.k22_3.k22_3_1;
  *
  * @author maxio
  */
+class A2204 {
+    int irgendwas;
+}
+
+class B2204 extends A2204 implements Runnable {
+    public void run() {
+        int i = 0;
+        while (true) {
+            if (Thread.interrupted()) {
+                break;
+            }
+            System.out.println(i++);
+        }
+    }
+}
+
 public class MyThread2204 {
-    
+    public static void main(String[] args) {
+        B2204 b = new B2204();
+        Thread t = new Thread(b);
+        t.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+        }
+        t.interrupt();
+    }
 }
