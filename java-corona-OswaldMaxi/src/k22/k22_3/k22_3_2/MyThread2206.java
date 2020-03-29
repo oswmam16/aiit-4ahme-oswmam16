e73e8119b59c4bc3c6d5e01014c4e942d58ce0c3
Oswald Maximilian <45564897+oswmam16@users.noreply.github.com>
2020-03-29 16:05:28 +0200

    Create MyThread2206.java

---
+++ java-corona-OswaldMaxi/src/k22/k22_3/k22_3_2/MyThread2206.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package k22.k22_3.k22_3_2;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author maxio
+ */
+
+public class MyThread2206 {
+    public static void main(String[] args) {
+        MyThread2205 th = new MyThread2205();
+        BufferedReader   in = new BufferedReader(
+                              new InputStreamReader(
+                              new DataInputStream(System.in)));
+        int num;
+ 
+        try {
+            while (true) {
+                System.out.print("Bitte eine Zahl eingeben: ");
+                System.out.flush();
+                num = (new Integer(in.readLine())).intValue();
+                if (num == -1) {
+                    break;
+                }
+                th.printPrimeFactors(num);
+            }
+        } catch (IOException e) {
+        }
+    }
+}
