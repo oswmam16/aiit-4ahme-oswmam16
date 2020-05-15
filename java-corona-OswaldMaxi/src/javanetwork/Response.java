d8a9c878349b22e99a51a4c8fbdb07f61662652b
Oswald Maximilian <45564897+oswmam16@users.noreply.github.com>
2020-05-15 10:46:12 +0200

    Response.jave
    Erzeugt und ausprogrammiert

--- java-corona-OswaldMaxi/src/javanetwork/Response.java
+++ java-corona-OswaldMaxi/src/javanetwork/Response.java
@@ -10,5 +10,40 @@ package javanetwork;
  * @author maxio
  */
 public class Response {
+    private boolean master;
+    private long count;
+    private boolean running;
+    private long time;
     
+    public boolean isMaster() {
+        return master;
+    }
+    
+    public void setMaster(boolean master) {
+        this.master = master;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+    
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
 }
