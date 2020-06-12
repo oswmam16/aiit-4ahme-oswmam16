cd983925075af07fc42b27d6fc6d6d266847c9b8
Oswald Maximilian <45564897+oswmam16@users.noreply.github.com>
2020-06-12 09:57:14 +0200

    ConnectionWorker updated
    abgeändert

--- java-corona-OswaldMaxi/src/javanetwork/ConnectionWorker.java
+++ java-corona-OswaldMaxi/src/javanetwork/ConnectionWorker.java
@@ -5,38 +5,66 @@
  */
 package javanetwork;
 
-import com.google.gson.Gson;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
+import javanetwork.Server.Response;
 import javax.swing.SwingWorker;
 
 /**
  *
  * @author maxio
  */
-public class ConnectionWorker extends SwingWorker<Object, Object> {
+public class ConnectionWorker extends SwingWorker<Object, Response> {
 
     private final Socket socket;
+    private javanetwork.gui.ClientGUI gui;
 
-    public ConnectionWorker(Socket socket) {
-        this.socket = socket;
+    public ConnectionWorker(int port, javanetwork.gui.ClientGUI gui) throws IOException {
+        this.socket = new Socket("127.0.0.1", port);
+        this.gui = gui;
     }
 
     @Override
     protected Object doInBackground() throws Exception {
-        try {
-            final OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
-            final Gson gson = new Gson();
-            
-        } catch(Exception ex) {
-            
+        while(true) {
+            Response resp = null;
+            publish(resp);
         }
-        return 0;
+//        try {
+//            final OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+//            final Gson gson = new Gson();
+//            Server s1 = new Server();
+//            final Server.Request req = s1.new Request();
+//            
+//            final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//            final String resp = reader.readLine();
+//            final Server.Response r = gson.fromJson(resp, Server.Response.class);
+//            
+//            if(!r.isMaster()) {
+//                req.setMaster(true);
+//            }
+//            
+//            final String reqString = gson.toJson(req, Server.Request.class);
+//            writer.write(reqString);
+//            writer.flush();
+//            
+//        } catch(Exception ex) {
+//            
+//        }
+//        return 0;
+    }
+
+    @Override
+    protected void process(List<Response> list) {
+        for(Response r : list) {
+            gui.handleResponse(r);
+        }
     }
 
     @Override
     protected void done() {
         super.done(); //To change body of generated methods, choose Tools | Templates.
     }
-     
+    
 }
